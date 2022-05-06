6be272b76e28bea192573aba96fa21fd43f11031
André Gama <toeoficial@outlook.com>
2022-05-06 20:55:56 -0300

    feat: adiciona alternância entre edições

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,38 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 
-const imgurLoader = ({ src }) => {
-  return `https://i.imgur.com${src}`
-}
+const editions = [
+  'editions/1.jpg',
+  'editions/2.png',
+  'editions/3.png'
+]
 
 export default function Home() {
+  const [currentEdition, setEdition] = useState(0);
+
   return (
     <div className="bg-gray-200">
       <Head>
         <title>Ribeirando - Edição Digital</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <img src="https://cdn.discordapp.com/attachments/752415841114390588/864374393543196672/Revista_Ribeirando_N3.png" alt="Ribeirando" className="mx-auto" />
+      <header className="mx-auto flex items-center justify-center space-x-4 bg-white border-b p-2">
+        {editions.map((edition, index) => (
+          <button
+            key={edition}
+            className={`text-md font-medium focus:outline-none transition-colors duration-300 rounded hover:bg-gray-200 p-2 ${currentEdition === index ? 'bg-gray-200' : ''}`}
+            onClick={() => setEdition(index)}
+          >
+            Edição {index + 1}
+          </button>
+        ))}
+      </header>
+      <img
+        src={editions[currentEdition]}
+        alt="Ribeirando"
+        className="mx-auto"
+      />
     </div>
-  )
+  );
 }
